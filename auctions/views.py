f6eebd44f56725136721dd02db3450fc93da9016
dennis dela torre <insyde.dedsec@gmail.com>
2020-08-09 16:37:49 +0800

    added comments on views.py

--- auctions/views.py
+++ auctions/views.py
@@ -7,72 +7,79 @@ from django.urls import reverse
 
 from .models import User, Listings, Categories, Watchlist
 
+# INDEX
 def index(request):
 
     return render(request, "auctions/index.html", {
-        "listings": Listings.objects.all()
+        "listings": Listings.objects.all() # RETURN ALL ACTIVE LISTINGS
     })
 
+# LISTING PAGE
 @login_required(login_url='login')
 def listing(request, list_id):
 
-    if request.method == "POST":
+    if request.method == "POST": # IF METHOD IS POST
 
-        Listings.objects.filter(pk=list_id).update(bid=request.POST["place_bid"])
-        Listings.objects.filter(pk=list_id).update(bidder=request.POST["user_id"])
+        Listings.objects.filter(pk=list_id).update(bid=request.POST["place_bid"]) # UPDATEDS THE CURRENT HIGHEST BID
+        Listings.objects.filter(pk=list_id).update(bidder=request.POST["user_id"]) # UPDATES THE CURRENT HIGHEST BIDDER/USER
 
-        HttpResponseRedirect(reverse("listing", args=(list_id,)))
+        HttpResponseRedirect(reverse("listing", args=(list_id,))) # REDIRECT TO LISTINGS WITH ARGS LISTING ID
 
     lists = Listings.objects.get(id=list_id)
     
     return render(request, "auctions/listing.html", {
         "listings": lists,
-        "users": User.objects.get(id=lists.creator),
-        "bidder": User.objects.get(id=lists.bidder),
-        "min": lists.bid + 1,
-        "watchlist": Watchlist.objects.filter(watchlist=list_id),
+        "users": User.objects.get(id=lists.creator), # RETURN CREATOR OF LISTINGS
+        "bidder": User.objects.get(id=lists.bidder), # RETURN THE CURRENT HIGHEST BIDDER/USER
+        "min": lists.bid + 1, # CURRENT BID AMOUNT PLUS 1 FOR ERROR CHECKING
+        "watchlist": Watchlist.objects.filter(watchlist=list_id), 
     })
 
+# CATEGORIES
 def categories(request):
     category = Categories.objects.all()
     return render(request, "auctions/categories.html", {
-        "categories": category
+        "categories": category # RETURNS ALL CATEGORIES
     })
 
+# CATTEGORY
 def category(request, category_id):
     category = Listings.objects.filter(categories=category_id)
     return render(request, "auctions/category.html", {
-        "categories": category
+        "categories": category # RETURNS SPECIFIC CATEGORY
     })
 
+# CLOSE LISTING PAGE
 def close(request):
 
-    if request.method == "POST":
+    if request.method == "POST":  # IF METHOD IS POST
 
-        delete = Listings.objects.filter(id=request.POST["listings_id"])
+        close = Listings.objects.filter(id=request.POST["listings_id"]) #FILTER IF EQUAL TO ID LISTING
 
-        delete.delete()
+        close.delete() # DELETE LISTING
 
         return HttpResponseRedirect(reverse("index"))
 
+# REMOVE WATCHLIST
 def remove(request):
 
-    remove = Watchlist.objects.filter(user=request.user.id, watchlist=request.POST["remove_id"])
-    remove.delete()
+    remove = Watchlist.objects.filter(user=request.user.id, watchlist=request.POST["remove_id"]) # FILTERS USER.ID AND LIST ID
+    remove.delete() # DELETE WATCHLIST
 
-    return HttpResponseRedirect(reverse("watchlist"))
+    return HttpResponseRedirect(reverse("watchlist")) # REDIRECT TO WATCHLIST
 
+# WATCHLIST
 @login_required(login_url='login')
 def watchlist(request):
 
-    if request.method == "POST":
+    if request.method == "POST": # IF METHOD IS POST
             
         watchlist = Watchlist(
             watchlist = request.POST["add_id"],
             user = request.user.id
         )
 
-        watchlist.save()
+        watchlist.save() # SAVE CREATED WATCHLIST
 
         return HttpResponseRedirect(reverse("watchlist"))
 
@@ -81,10 +88,11 @@ def watchlist(request):
         "listings": Listings.objects.all()
     })
 
+# CREATEPAGE
 @login_required(login_url='login')
 def create(request):
 
-    if request.method == "POST": 
+    if request.method == "POST": # IF METHOD IS POST
 
         lists = Listings(
             title=request.POST["title"], 
@@ -96,14 +104,15 @@ def create(request):
             categories=Categories.objects.get(category=request.POST["category"])
             )
             
-        lists.save()
+        lists.save() # SAVE NEWLY CREATED LISTING
 
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("index")) # REDIRECT TO INDEX
 
     category = Categories.objects.all()
     return render(request, "auctions/create.html", {
-        "categories": category
+        "categories": category # RETURN ALL CATEGORIES
     })
+
 
 def login_view(request):
     if request.method == "POST":
