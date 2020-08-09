af74e1c42ce46c981d6b7a21741a6075c3adf937
dennis dela torre <insyde.dedsec@gmail.com>
2020-08-09 16:42:04 +0800

    added comments on htmls

--- auctions/models.py
+++ auctions/models.py
@@ -5,28 +5,28 @@ class User(AbstractUser):
     pass
 
 class Categories(models.Model):
-    category = models.CharField(max_length=32)
+    category = models.CharField(max_length=32) # CATEGORIES
 
     def __str__(self):
         return self.category
 
 class Listings(models.Model):
-    title = models.CharField(max_length=32)
-    description = models.CharField(max_length=256, null=True)
-    time = models.DateTimeField(auto_now_add=True, null=True)
-    image = models.URLField(max_length=2048, null=True)
-    bid = models.IntegerField()
-    categories = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="type")
-    comment = models.CharField(max_length=256, null=True, blank=True)
-    creator = models.IntegerField()
-    bidder = models.IntegerField()
+    title = models.CharField(max_length=32) # TITLE
+    description = models.CharField(max_length=256, null=True) # DESCRIPTION
+    time = models.DateTimeField(auto_now_add=True, null=True) # DATE AND TIME
+    image = models.URLField(max_length=2048, null=True) # IMAGE URL 
+    bid = models.IntegerField() # CURRENT BID
+    categories = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="type") # CATEGORIES
+    comment = models.CharField(max_length=256, null=True, blank=True) 
+    creator = models.IntegerField() # CREATOR OF THE LISTING
+    bidder = models.IntegerField() # CURRENT HIGHEST BIDDER
 
     def __str__(self):
         return f"{self.id} - {self.title}: ${self.bid} ({self.categories}) {self.time}"
 
 class Watchlist(models.Model):
-    watchlist = models.IntegerField()
-    user = models.IntegerField(default=0)
+    watchlist = models.IntegerField() # LISTING ID
+    user = models.IntegerField(default=0) # USER ID
 
     def __int__(self):
         return self.watchlist
